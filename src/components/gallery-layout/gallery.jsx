7d41b7c77b25896c3ea4bfe725dc8c157d004bcd
JoteNati <joejona14@gmail.com>
2024-11-22 17:57:59 +0300

    error handle

--- src/components/gallery-layout/gallery.jsx
+++ src/components/gallery-layout/gallery.jsx
@@ -6,49 +6,61 @@ function Gallery() {
 
   const [loading, setLoading] = useState([]);
   const [images, setImages] = useState([false]);
+  const [errorMessage, setErrorMessage] = useState('');
   
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false)
-    }, 2000)
-  }, [])
-
+  
   useEffect(() => {
     const getAllImages = async () => {
       try {
-        const allImages = await axios.get('https://api.unsplash.com/photos?page=12&per_page=100&client_id=SE16Ng2YTGykJz_XFcNMmPjp40u4fL6lZbhw5zR6omU');
+        const allImages = await axios.get('https://api.unsplash.com/photos?page=100&per_page=150&client_id=SE16Ng2YTGykJz_XFcNMmPjp40u4fL6lZbhw5zR6omU');
         console.log('Result ', allImages)
         setImages(allImages.data);
+
+        setLoading(true);
+        setTimeout(() => {
+          setLoading(false)
+        }, 2000)
+
       } catch (error) {
         console.log(error);
+        setErrorMessage(error.message);
       }
     };
-
+    
     getAllImages();
   }, [])
 
-
   return (
     <div>
-      {
-        loading ? 
-        <div className='loading'>
-          <h3>Images Loading ...</h3>
-        </div>
-        : 
-        <div className='gallery-container'>
-        { images.map(image => {
-          return (
-            <div className="gallery-card">
-              <img src={image.urls.small} alt={image.alt_description} />
-              <span>{image.user.first_name} {image.user.last_name}</span>
-            </div>
-          );
-        }
-        )}
-      </div>
-      }
+          { 
+              errorMessage !== "" ?
+              <>
+                <div className='error'>
+                  <h3>Something went wrong: {errorMessage}!</h3>
+                </div>
+              </>
+          : 
+              <>
+                {
+                  loading ? 
+                  <div className='loading'>
+                    <h3>Images Loading ...</h3>
+                  </div>
+                  : 
+                  <div className='gallery-container'>
+                  { images.map(image => {
+                    return (
+                      <div className="gallery-card">
+                        <img src={image.urls.small} alt={image.alt_description} />
+                        <span>{image.user.first_name} {image.user.last_name}</span>
+                      </div>
+                    );
+                  }
+                  )}
+                  </div>
+                }
+              </>  
+          }
     </div>
   );
 }
